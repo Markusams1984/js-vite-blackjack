339be6d6c2813b3dbf9e3deea82689671bf0b6a7
Marco Munoz <marco.a.m.s.84@gmail.com>
2026-04-29 11:24:58 -0400

    error en crear-carta-html.js se quito el / de assests

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -12,9 +12,9 @@ export const crearCartaHTML = ( carta ) => {
 
 // <img class="carta" src="assets/cartas/2C.png">
         const imgCarta = document.createElement('img');
-        imgCarta.src = `/assets/cartas/${ carta }.png`;
+        imgCarta.src = `assets/cartas/${ carta }.png`;
         imgCarta.classList.add('carta');
-    
+
     return imgCarta;
 
 }
